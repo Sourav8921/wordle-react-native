d016204cfe37a637f248f0eaf47dd5b1131ede2f
souravwc <sourav.woodenclouds@gmail.com>
2024-11-29 23:40:05 +0530

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,21 @@
+import { StyleSheet, Text, View } from "react-native";
+import Icon from '@/assets/images/wordle-icon.svg'
+
+export default function Index () {
+  return (
+    <View
+      style={styles.container}
+    >
+      <Icon width={100} height={100} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+})
