c26f7f1e83393bea4ff35cac73809dad4e2d3296
Sourav8921 <souravramesh39@gmail.com>
2024-12-02 23:37:04 +0530

    building 1st page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,26 @@
 import { Stack } from "expo-router";
+import { useFonts, FrankRuhlLibre_800ExtraBold, FrankRuhlLibre_500Medium, FrankRuhlLibre_900Black } from '@expo-google-fonts/frank-ruhl-libre';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from "react";
 
-export default function RootLayout() {
+//load the fonts before hiding the splash screen
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout () {
+  const [fontsLoaded] = useFonts({
+    FrankRuhlLibre_800ExtraBold,
+    FrankRuhlLibre_500Medium,
+    FrankRuhlLibre_900Black,
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
   return <Stack />;
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,26 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import Icon from '@/assets/images/wordle-icon.svg'
+import { FrankRuhlLibre_800ExtraBold } from "@expo-google-fonts/frank-ruhl-libre";
+import { Link } from "expo-router";
 
 export default function Index () {
   return (
-    <View
-      style={styles.container}
-    >
-      <Icon width={100} height={100} />
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Icon width={100} height={100} />
+        <Text style={styles.title}>Wordle</Text>
+        <Text style={styles.text}>Get six chances to guess a 5-letter word.</Text>
+      </View>
+      <View style={styles.menu}>
+        <Link href={'/game'}>
+          <TouchableOpacity>
+            <Text>Play</Text>
+          </TouchableOpacity>
+        </Link>
+      </View>
+      <View style={styles.footer}>
+        <Text style={styles.text}>Made by Sourav</Text>
+      </View>
     </View>
   );
 }
@@ -14,8 +28,23 @@ export default function Index () {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
     justifyContent: 'center',
+    paddingHorizontal: 50,
+    gap: 50,
   },
-
+  header: {
+    alignItems: 'center',
+    gap: 10,
+  },
+  title: {
+    fontSize: 40,
+    fontFamily: "FrankRuhlLibre_800ExtraBold",
+  },
+  text: {
+    fontSize: 26,
+    textAlign: 'center',
+    fontFamily: "FrankRuhlLibre_500Medium",
+  },
+  menu: {},
+  footer: {},
 })
